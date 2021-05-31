fe08ee3d708fb584fee1d3b709eb0c0c632cad8c
ved.ma <sleep.dora@gmail.com>
2021-05-31 09:37:11 +0300

    Этап I. Формирование структуры базы Hibernate

---
+++ src/main/java/main/model/User.java
@@ -0,0 +1,39 @@
+package main.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "is_moderator", nullable = false)
+    private boolean isModerator;
+
+    @DateTimeFormat(pattern = "yyyy.MM.dd HH-mm")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH-mm")
+    @Column(name = "reg_time", nullable = false)
+    private Date regTime;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "photo")
+    private String photo;
+}
